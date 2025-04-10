f44999789bf237680db384b3387e1779c6b0c728
hnahtneyugn <tanguyenthanh1969@gmail.com>
2025-04-11 00:11:32 +0700

    tried to move to PPO

--- a2c_lunar_lander.py
+++ a2c_lunar_lander.py
@@ -21,37 +21,36 @@ env = gym.make(
 # Training parameters
 space_dim = env.observation_space.shape[0]
 action_dim = env.action_space.n
-n_epochs = 5000
+n_epochs = 1000
 eval_epochs = 1000
 gamma = 0.99
-alpha = 0.001
-alpha_c = 0.002
+alpha = 0.0003
+alpha_c = 0.0005
 target_steps = 1000
-entropy_coefficient_start = 0.1
-entropy_coefficient_end = 0.01
-entropy_coefficient_decay = 0.995
-entropy_coefficient = entropy_coefficient_start
+entropy_coef = 0.01
 pbar = tqdm(range(n_epochs))
 ebar = tqdm(range(eval_epochs))
 max_ave_steps = 0
 max_ave_rewards = 0 
 n_steps = []
 rewards = []
+actor_losses = []
+critic_losses = []
 
 # The network to select an action
 ActorNetwork = nn.Sequential(
-    nn.Linear(space_dim, 256),
+    nn.Linear(space_dim, 128),
     nn.LeakyReLU(),
-    nn.Linear(256, 128),
+    nn.Linear(128, 128),
     nn.LeakyReLU(),
     nn.Linear(128, action_dim)
 )
 
 # The network to get value of a state
 CriticNetwork = nn.Sequential(
-    nn.Linear(space_dim, 256),
+    nn.Linear(space_dim, 128),
     nn.LeakyReLU(),
-    nn.Linear(256, 128),
+    nn.Linear(128, 128),
     nn.LeakyReLU(),
     nn.Linear(128, 1)
 )
@@ -63,7 +62,7 @@ critic_optimizer = optim.Adam(CriticNetwork.parameters(), lr=alpha_c)
 # Training
 for t in pbar:
     state, _ = env.reset()          # Get s_t state
-    state = torch.FloatTensor(state)
+    state = torch.FloatTensor(state).unsqueeze(0)
     count = 0
     epoch_reward = 0
     done = False
@@ -76,17 +75,20 @@ for t in pbar:
         log_prob = dist.log_prob(action)
 
         next_state, reward, done, truncated, _ = env.step(action=action.item())        # Get state s_t+1, reward r_t+1 from environment
-        next_state = torch.FloatTensor(next_state)
+        next_state = torch.FloatTensor(next_state).unsqueeze(0)
         done = done or truncated
 
-        advantage = reward + (1 - done) * gamma * CriticNetwork(next_state) - CriticNetwork(state)   # Calculate Advantage (TD Error)
+        value = CriticNetwork(state)
+        next_value = CriticNetwork(next_state).detach()
+        target = reward + (1 - done) * gamma * next_value   # Calculate TD Target
+        advantage = (target - value).detach()               # Calculate Advantage (TD Error)
 
-        critic_loss = 0.5 * advantage**2                    # Critic loss using advantage
-        actor_loss = -log_prob * advantage.detach()         # Actor loss using advantage and log probability of distribution
+        critic_loss = F.mse_loss(value, target)                 # Critic loss using advantage
+        actor_loss = -log_prob * advantage            # Actor loss using advantage and log probability of distribution
 
         entropy = dist.entropy()            
-        actor_loss = actor_loss - entropy_coefficient * entropy        # Entropy regularization trick
-        
+        actor_loss = actor_loss - entropy_coef * entropy        # Entropy regularization trick
+
         actor_optimizer.zero_grad()         # Update Actor
         actor_loss.backward()
         actor_optimizer.step()
@@ -100,9 +102,9 @@ for t in pbar:
         count += 1                          # Count number of steps     
 
     rewards.append(epoch_reward)
+    actor_losses.append(actor_loss.detach().item())
+    critic_losses.append(critic_loss.detach().item())
     n_steps.append(count)
-
-    entropy_coefficient = max(entropy_coefficient_end, entropy_coefficient_start * entropy_coefficient_decay)
 
     ave_steps = np.mean(n_steps[-50:])
     ave_rewards = np.mean(rewards[-50:])
@@ -115,9 +117,14 @@ for t in pbar:
     
     pbar.set_description(f"Epoch {t+1} Average steps: {ave_steps}, Highest step: {max_ave_steps}, Average reward: {ave_rewards}, Highest reward: {max_ave_rewards}")
 
+torch.save(ActorNetwork.state_dict(), "models/final_train_actor_network.pth")
+torch.save(CriticNetwork.state_dict(), "models/final_train_critic_network.pth")
+print("Saved final model after training")
+
 # Evaluation in 1000 epochs using highest softmax action and save to a video file    
-env_video = RecordVideo(env, "./a2c_lunar_lander_5000_0.001_0.002")
+env_video = RecordVideo(env, "./a2c_ll_1k_3e4_5e4_128_128_test1")
 total_evaluation_reward = np.array([])
+best_accuracy = 0
 
 for epoch in ebar:
     state, _ = env_video.reset()
@@ -137,8 +144,15 @@ for epoch in ebar:
     if epoch % 100 == 0:
             print(f"Finished episode {epoch}, Reward: {epoch_reward:.1f}")
 
-    pbar.set_description(f"Epoch {epoch+1} Epoch reward: {epoch_reward}")
+    ebar.set_description(f"Epoch {epoch+1} Epoch reward: {epoch_reward}")
 
-print(f"Accuracy: {np.count_nonzero(total_evaluation_reward[total_evaluation_reward >= 200]) / eval_epochs * 100}%")
+accuracy = np.count_nonzero(total_evaluation_reward[total_evaluation_reward >= 200]) / eval_epochs * 100
+if accuracy > best_accuracy:
+    best_accuracy = accuracy
+    torch.save(ActorNetwork.state_dict(), f"models/best_eval_actor_network_{best_accuracy}.pth")
+    torch.save(CriticNetwork.state_dict(), f"models/best_eval_critic_network_{best_accuracy}.pth")
+    print(f"Saved best evaluation model with accuracy: {best_accuracy:.1f}%")
+
+print(f"Accuracy: {accuracy}%")
 
 env_video.close()
